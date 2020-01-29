62afddd901ffa7751c4ea93723ff372b91bffc51
purplecare <tsiu086@uottawa.ca>
2020-01-28 22:16:57 -0500

    Add Summary Page

--- app/src/main/java/com/example/sampletest/Questions.java
+++ app/src/main/java/com/example/sampletest/Questions.java
@@ -1,14 +1,25 @@
 package com.example.sampletest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import android.app.*;
+import android.widget.Button;
 
 public class Questions extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questions);
+        Button button=findViewById(R.id.submitButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Summary.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
