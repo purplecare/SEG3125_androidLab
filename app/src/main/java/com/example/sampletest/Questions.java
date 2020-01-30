5352c24fe4ca3d696ad584a680b4f446dde46fd3
purplecare <tsiu086@uottawa.ca>
2020-01-30 17:24:21 -0500

    Add more detail for summary page and tried implement json file

--- app/src/main/java/com/example/sampletest/Questions.java
+++ app/src/main/java/com/example/sampletest/Questions.java
@@ -1,13 +1,18 @@
 package com.example.sampletest;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-
+import java.io.*;
+import org.json.JSONException;
 import android.app.*;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 public class Questions extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +22,38 @@ public class Questions extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Summary.class);
+                intent.putExtra("result", 40);
                 startActivity(intent);
             }
 
         });
+        try {
+            JSONObject question = new JSONObject(loadJSONFromAsset());
+            String question_son= question.getString("CSI1102");
+            //JSONObject classes = question.getJSONObject(question_Json);
+            //String question_1=classes.getString("questions_1");
+            //String question_body=question_1.getString("body");
+
+            TextView content=findViewById(R.id.questionContent);
+            content.setText(question_son);
+
+        }
+        catch (JSONException e) {}
+
+    }
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("questions.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
--- app/src/main/java/com/example/sampletest/Summary.java
+++ app/src/main/java/com/example/sampletest/Summary.java
@@ -1,11 +1,37 @@
 package com.example.sampletest;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Summary extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.summary);
+        int marks= getIntent().getExtras().getInt("result");
+        TextView passed= findViewById(R.id.summary_passed);
+        TextView percentage=findViewById(R.id.summary_percentage);
+        percentage.setText(String.valueOf(marks)+"%");
+        if (marks>50){
+           passed.setText("Passed");
+        }
+        else{
+            passed.setText("Failed");
+            passed.setTextColor(Color.RED);
+            percentage.setTextColor(Color.RED);
+        }
+
+        Button button=findViewById(R.id.restart_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
